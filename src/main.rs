c30085a688d74bebc9c4b581000b755092fff1ec
Simon Bertrand <simonbertrand.contact@gmail.com>
2021-08-31 01:12:44 +0200

    Base fundamentals

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,133 @@
+
 use std::convert::TryInto;
 use std::fmt::Display;
-use std::ops::Mul;
+use core::ops::Mul;
+
+#[derive(Clone, Copy)]
+enum Type {
+    Int(i32),
+    Float(f64),
+    Bool(bool),
+}
+
+impl std::fmt::Display for &Type {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Type::Int(a) => Ok(print!("{}",a)),
+            Type::Float(a) => Ok(print!("{}",a)),
+            Type::Bool(a)  => Ok(print!("{}",a)),
+        }
+        
+    }
+}
 
 
 
 
-
-
-
-struct Matrix<T> {
-    values: Vec<T>,
+struct Matrix {
+    values: Vec<Type>,
     shape : (i32,i32),
 
 }
+
+impl Matrix {
+    fn new(shape1 : i32, shape2 : i32) -> Matrix { Matrix {values : vec![Type::Int(0); ((shape1*shape2) as usize) ], shape:(shape1,shape2)}}
+    fn ones(shape1 : i32, shape2 : i32) -> Matrix { Matrix {values : vec![Type::Int(1); ((shape1*shape2) as usize) ], shape:(shape1,shape2)}}
+    fn eye(shape : i32) -> Matrix { Matrix {
+    values:
+    {let mut value : Vec<Type> = Vec::new();
+        for i in 0..shape{
+            for j in 0..shape{
+                if i==j {
+                    value.push(Type::Int(1));
+                }
+                else {
+                    value.push(Type::Int(0));
+                }     
+            }
+        }
+    value},
+    shape : (shape,shape),
+    }}
+
+    fn from_slice(slice: &[Type], shape1:i32, shape2:i32) -> Matrix { 
+        
+        if slice.len() != (shape1*shape2).try_into().unwrap() {
+            println!("\nfn from_slice : The lenght of the slice is not equal to the product of both shapes.");
+            return Matrix{values:vec![Type::Int(0)], shape:(1,1)}
+        }
+        else {
+            Matrix {values : {
+                let mut v : Vec<Type> = Vec::new();
+                for el in slice{
+                    
+                    v.push(*el);
+                }
+                v
+            }, shape:(shape1,shape2)} }
+        }
+}
+
+impl Matrix{
+    fn show(&self) {
+        let mut i=0;
+        println!("");
+        print!("| ");
+        for val in self.values.iter() {      
+            if i == self.shape.1 {
+                println!("|");
+                print!("| ");
+                i=0
+            }
+            print!("{} ", val);
+            i+=1;
+        }
+        println!("|");
+    }
+}
+
+
+
+
+
+
+/*
+
+trait Mat { fn show(&self); }
+impl Mat for Matrix<i32> { fn show(&self) {println!("256")} }
+impl Mat for Matrix<f64> { fn show(&self) {println!("384")} }
+impl Mat for Matrix<bool> { fn show(&self) {println!("512")} }
+
+impl Mat for MatDataType {
+    fn show(&self) {
+        use MatDataType::*;
+        match *self {
+            Int(ref M )   => M.show(),
+            Float(ref M)   => M.show(),
+            Bool(ref M) => M.show(),
+        }
+    }
+}
+
+
+
+impl Matrix<i32> {
+    fn new(shape1:i32, shape2:i32) -> Matrix<i32> {
+        Matrix::<i32> { values: vec![0;((shape1*shape2) as usize).try_into().unwrap()] , shape: (shape1,shape2)}
+    }
+
+}
+
+*/
+
+
+
+
+/*
+impl Mat {
+    fn new(self, str: str) ->  {}
+}
+
 
 /* Constructors */
 impl<T: Clone> Matrix<T> {
@@ -58,7 +173,7 @@ impl<T: Display> Matrix<T> {
         let mut i=0;
         println!("");
         print!("| ");
-        for val in &self.values {      
+        for val in self.values.iter() {      
             if i == self.shape.1 {
                 println!("|");
                 print!("| ");
@@ -100,33 +215,30 @@ impl<T: Copy +  Clone + std::ops::Add<Output = T>  + std::ops::Mul<Output = T>> 
         shape:self.shape,
         }
     }
-    fn mul(&self, scal : T) -> Matrix<T> 
-    where T: std::ops::Mul<Output=T>{
-        Matrix::<T>{
-        values:
-            {
-            let mut r : Vec<T> = Vec::new();
-            for i in 0..self.values.len() {
-                r.push(scal*self.values[i]);
-            }
-            r},
-        shape:self.shape,
-        }
-    }
-
-    fn minus(&self, Vec1 : Matrix<T>) -> Matrix<T> {
-       self.add(Vec1.mul(T))
-    }
 
 }
 
 
+impl<T : Copy +  std::ops::Mul<Output = T>> Mul<T> for Matrix<T> {
+    type Output = Self;
+    fn mul(self, rhs: T) -> Self::Output {
+        Self { values: self.values.iter().map(|v| *v * rhs).collect(), shape:self.shape}
+    }
+}
 
+*/
 
 fn main() {
+
+    let Mat = Matrix::from_slice(&[Type::Int(1),Type::Int(10),Type::Float(1.2),Type::Bool(true)],2,2);
+    Mat.show();
+    /*
     let mut v = Matrix::<f64>::eye(6);
     let mut u = Matrix::<i32>::ones(6,6);
-    (v.add(u)).mul(2).show();
+    */
+
+
+    //((v.add(u)).mul(-1)).show();
 
 
 
