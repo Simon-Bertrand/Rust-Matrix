2145fddefe608d7c80c526bfbeac6e1a490d8aef
Simon-Bertrand <simonbertrand.contact@gmail.com>
2021-09-15 15:51:27 +0200

    Adding operations

--- src/matrix/operators.rs
+++ src/matrix/operators.rs
@@ -7,6 +7,8 @@ use std::ops::Div;
 
 use crate::matrix::Matrix;
 
+
+
 impl<T : Mul + Copy> Mul<T> for &Matrix<T>
 {
     type Output = Matrix<T::Output>;
@@ -47,6 +49,143 @@ macro_rules! sub_impl {
 }
 
 sub_impl! { usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128 f32 f64 }
+
+
+
+
+
+
+
+impl<T : Add + Copy> Add<T> for &Matrix<T>
+{
+    type Output = Matrix<T::Output>;
+    fn add(self, rhs: T) -> Matrix<T::Output> {
+        Matrix::<T::Output>{ 
+            values: {
+                let mut r: Vec<T::Output> = Vec::with_capacity(((&self).shape.0*(&self).shape.1) as usize); 
+                for el in (&self).values.iter() {
+                    r.push(*el + rhs)
+                }
+                r},
+            shape:self.shape}    
+        }
+}
+impl<T : Add + Copy> Add<T> for Matrix<T>
+{
+    type Output = Matrix<T::Output>;
+    fn add(self, rhs: T) -> Matrix<T::Output> {&self + rhs}
+}
+
+
+
+macro_rules! sub_impl {
+    ($($t:ty)*) => ($(
+        impl Add<&Matrix<$t>> for $t {
+            type Output = Matrix<$t>;
+            fn add(self, rhs: &Matrix<$t>) -> Matrix<$t>{
+                rhs + (self)
+            }
+        }
+        impl Add<Matrix<$t>> for $t {
+            type Output = Matrix<$t>;
+            fn add(self, rhs: Matrix<$t>) -> Matrix<$t>{
+                &rhs + (self)
+            }
+        }
+    )*)
+}
+
+sub_impl! { usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128 f32 f64 }
+
+
+
+
+
+
+impl<T : Sub + Copy> Sub<T> for &Matrix<T>
+{
+    type Output = Matrix<T::Output>;
+    fn sub(self, rhs: T) -> Matrix<T::Output> {
+        Matrix::<T::Output>{ 
+            values: {
+                let mut r: Vec<T::Output> = Vec::with_capacity(((&self).shape.0*(&self).shape.1) as usize); 
+                for el in (&self).values.iter() {
+                    r.push(*el - rhs)
+                }
+                r},
+            shape:self.shape}    
+        }
+}
+impl<T : Sub + Copy> Sub<T> for Matrix<T>
+{
+    type Output = Matrix<T::Output>;
+    fn sub(self, rhs: T) -> Matrix<T::Output> {&self - rhs}
+}
+
+macro_rules! sub_impl {
+    ($($t:ty)*) => ($(
+        impl Sub<&Matrix<$t>> for $t {
+            type Output = Matrix<$t>;
+            fn sub(self, rhs: &Matrix<$t>) -> Matrix<$t>{
+                rhs - (self)
+            }
+        }
+        impl Sub<Matrix<$t>> for $t {
+            type Output = Matrix<$t>;
+            fn sub(self, rhs: Matrix<$t>) -> Matrix<$t>{
+                &rhs - (self)
+            }
+        }
+    )*)
+}
+
+sub_impl! { usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128 f32 f64 }
+
+
+
+
+
+impl<T : Div + Copy> Div<T> for &Matrix<T>
+{
+    type Output = Matrix<T::Output>;
+    fn div(self, rhs: T) -> Matrix<T::Output> {
+        Matrix::<T::Output>{ 
+            values: {
+                let mut r: Vec<T::Output> = Vec::with_capacity(((&self).shape.0*(&self).shape.1) as usize); 
+                for el in (&self).values.iter() {
+                    r.push(*el / rhs)
+                }
+                r},
+            shape:self.shape}    
+        }
+}
+impl<T : Div + Copy> Div<T> for Matrix<T>
+{
+    type Output = Matrix<T::Output>;
+    fn div(self, rhs: T) -> Matrix<T::Output> {&self / rhs}
+}
+
+macro_rules! sub_impl {
+    ($($t:ty)*) => ($(
+        impl Div<&Matrix<$t>> for $t {
+            type Output = Matrix<$t>;
+            fn div(self, rhs: &Matrix<$t>) -> Matrix<$t>{
+                rhs / (self)
+            }
+        }
+        impl Div<Matrix<$t>> for $t {
+            type Output = Matrix<$t>;
+            fn div(self, rhs: Matrix<$t>) -> Matrix<$t>{
+                &rhs / (self)
+            }
+        }
+    )*)
+}
+
+sub_impl! { usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128 f32 f64 }
+
+
+
 
 /*
 
